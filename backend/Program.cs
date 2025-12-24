d31a5556e83f4baaa5114eccb989c99d9f42b30b
Habibulloh2102 <hafizulloachilov@gmail.com>
2025-12-24 14:36:50 +0300

    fourth

--- backend/Program.cs
+++ backend/Program.cs
@@ -18,9 +18,9 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("git commit", () =>
+app.MapGet("git status", () =>
 {
-    return "делает их «снимок» (snapshot) и навсегда сохраняет его в истории проекта";
+    return "это статус";
 })
 .WithName("GetWeatherForecast");
 
