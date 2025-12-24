eadd927cc481fd5e53bbef46eaec70a9c2dc4045
Habibulloh2102 <hafizulloachilov@gmail.com>
2025-12-24 14:17:56 +0300

    second

--- backend/Program.cs
+++ backend/Program.cs
@@ -18,17 +18,9 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("git add .", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return "Подготовь эти файлы, я хочу включить их в следующий коммит";
 })
 .WithName("GetWeatherForecast");
 
